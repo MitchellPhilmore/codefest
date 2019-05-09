d482ede5b7ec225bbf2a582ee0d51e8bb207a2e8
mitchellphilmore <mitchellphilmore@gmail.com>
2019-05-09 02:14:34 -0400

    setup pwa

--- public/src/index.js
+++ public/src/index.js
@@ -10,3 +10,12 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+});
