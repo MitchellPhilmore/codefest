b4068f6b4ba0d4e22b57efacee27d14719d24687
unknown <mitchellphilmore@gmail.com>
2019-05-14 23:42:22 -0400

    Removed service worker function

--- public/src/index.js
+++ public/src/index.js
@@ -11,11 +11,6 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
 
-let deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-});
+
+
